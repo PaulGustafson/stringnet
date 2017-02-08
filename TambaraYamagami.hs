49a8c6f91e86f4e05a0a3ce709cf74f583f05b9b
Paul Gustafson <paul.gustafson@gmail.com>
2017-02-08 12:23:16 -0600

    Started to change typing for TambaraYamagami away from lists/matrices to functions

--- TambaraYamagami.hs
+++ TambaraYamagami.hs
@@ -11,8 +11,12 @@
 -- Kenichi Shimizu. Frobenius-Schur indicators in Tambara-Yamagami
 -- categories.
 --
--- TODO: write unit tests for important methods
+-- FIXME: Change Scalar class to include tau exponent
+--        Objects should be represented by multiplicity lists
+--        so that they are represented uniquely.
+--        Morphisms should be maps of indexed objects
 --
+-- TODO: write unit tests for important methods
 --
 -- TODO: implement simple reductions for scalars,
 -- e.g. \sum_{i=0}^{p-1} \zeta^i = 0
@@ -22,11 +26,11 @@
 
 module TambaraYamagami where
 
-import qualified Data.List.NonEmpty as N
-import qualified Data.Matrix as M
-import           Data.Semigroup
-import qualified Data.Vector        as V
-import qualified Data.List          as L
+import Data.List.NonEmpty as N
+import Data.Matrix as M
+import Data.Semigroup
+import Data.Vector        as V
+import Data.List          as L
 import qualified Stringnet          as S
 import Data.Group
 
@@ -37,36 +41,41 @@ nu = 1
 
 -- group element, assuming cyclicity
 -- dependent types would be nice here
-newtype GroupElement = GroupElement Int deriving (Show)
+newtype AElement = AElement Int deriving (Show)
 
-instance Eq GroupElement where
-  (GroupElement a) == (GroupElement b) = (a `mod` order) == (b `mod` order)
+instance Eq AElement where
+  (AElement a) == (AElement b) = (a `mod` order) == (b `mod` order)
 
-instance Monoid GroupElement where
-  (GroupElement e1) `mappend` (GroupElement e2) = GroupElement $ (e1 + e2) `mod` order
-  mempty = GroupElement 0
+instance Monoid AElement where
+  (AElement e1) `mappend` (AElement e2) = AElement $ (e1 + e2) `mod` order
+  mempty = AElement 0
     
-instance Group GroupElement where
-  invert (GroupElement e) = GroupElement  $ (-e) `mod` order
+instance Group AElement where
+  invert (AElement e) = AElement  $ (-e) `mod` order
 
 -- carrier set for the group
-group :: [GroupElement]
-group = Prelude.map GroupElement [0..(order - 1)]
+group :: [AElement]
+group = Prelude.map AElement [0..(order - 1)]
 
-plus :: GroupElement -> GroupElement -> GroupElement
+plus :: AElement -> AElement -> AElement
 plus = mappend
 
 
-newtype RootOfUnity = RootOfUnity GroupElement deriving (Eq, Monoid, Group)
+newtype RootOfUnity = RootOfUnity AElement deriving (Eq, Monoid, Group)
 
 -- A scalar is an algebraic integer over the cyclotomic field corresponding
 -- to the order of the group.
-newtype Scalar =  Scalar [Int]
+data Scalar =  Scalar 
+  { coeff :: [Int]
+  , tauExp :: Int
+  } deriving (Show, Eq)
+
+  
 
 coefficient :: Scalar -> RootOfUnity -> Int
-coefficient (Scalar coeffs) r =
+coefficient (Scalar coeffs _) r =
   case r of RootOfUnity g ->
-              case g of GroupElement i ->
+              case g of AElement i ->
                           coeffs !! i
                                         
 toCoeffList :: Scalar -> [Int]
@@ -109,7 +118,7 @@ instance Num Scalar where
 
 fromFunction :: (RootOfUnity -> Int) -> Scalar
 fromFunction f =
-  Scalar [f $ RootOfUnity $ GroupElement $ x | x <- [0..(order - 1)]]
+  Scalar [f $ RootOfUnity $ AElement $ x | x <- [0..(order - 1)]]
 
 fromBag :: [RootOfUnity] -> Scalar
 fromBag rs = fromFunction $ \r ->
@@ -125,15 +134,15 @@ fromRootOfUnity x = fromFunction $ \y ->
 -- Quadratic gauss sum
 -- Currently only works if order = 1 (mod 4)
 -- tauI :: Scalar
--- tauI =  nu * fromBag [RootOfUnity $ GroupElement (n^2) | n <- [0..(order - 1)]]
+-- tauI =  nu * fromBag [RootOfUnity $ AElement (n^2) | n <- [0..(order - 1)]]
 
 
 
-chi :: GroupElement -> GroupElement -> Scalar
-chi (GroupElement e1) (GroupElement e2) = fromRootOfUnity $ RootOfUnity $ GroupElement $ (e1*e2) `mod` order
+chi :: AElement -> AElement -> Scalar
+chi (AElement e1) (AElement e2) = fromRootOfUnity $ RootOfUnity $ AElement $ (e1*e2) `mod` order
 
-chiI :: GroupElement -> GroupElement -> Scalar
-chiI (GroupElement e1) (GroupElement e2) = fromRootOfUnity $ RootOfUnity $ GroupElement $ (-e1*e2) `mod` order
+chiI :: AElement -> AElement -> Scalar
+chiI (AElement e1) (AElement e2) = fromRootOfUnity $ RootOfUnity $ AElement $ (-e1*e2) `mod` order
 
 
 data SimpleObject =
@@ -141,49 +150,75 @@ data SimpleObject =
   M
 
   -- Group-element-indexed simple objects
-  | GE GroupElement
+  | AE AElement
                   deriving (Show,Eq)
 
+allSimpleObjects = [M] ++ (map AE group)
 
-data Object = SumO {
-  summandsO :: [SimpleObject]
-  } deriving (Show)
+data Object = Object
+  { multiplicity :: SimpleObject -> Int
+  } 
 
 so :: SimpleObject -> Object
-so x = SumO [x]
+so x = \y ->
+  if x == y
+  then 1
+  else 0
+
+
+groupObject :: Object
+groupObject = Object $ map AE group
 
 -- (tau ^ tauExponent) * matrix
+-- Should be a map betweeen two simple objects
 data TauMatrix =  TauMatrix
-  { matrix :: M.Matrix Scalar
+  { domain :: Object
+  , codomain :: Object  
+  , matrix :: M.Matrix Scalar
   , tauExponent :: Sum Int
   } deriving Show
 
 -- Matrices of scalars mappend identity maps
 -- TODO: convert this to a dependent type Domain -> Codomain -> Scalar
-data Morphism = Morphism {
-  summandsM :: [(TauMatrix)]
+data Morphism = Morphism 
+  { domain   :: Object
+  , codomain :: Object
+  , 
   } deriving Show
 
+
+-- TODO: newtype wrapping
 instance Semigroup Morphism where
   m1 <>  m2 = Morphism $ (summandsM m1) ++ (summandsM m2)
 
 
-toMorphism :: M.Matrix Scalar -> Morphism
-toMorphism m = Morphism [TauMatrix m 0]
+-- toMorphism :: M.Matrix Scalar -> Morphism
+-- toMorphism m = Morphism [TauMatrix m 0]
 
 idMorphism :: Object -> Morphism
-idMorphism o = Morphism [TauMatrix (M.diagonal 0 $ V.replicate (length $ summandsO o) 1) 0]
+idMorphism o = Morphism
+  [TauMatrix o o (M.diagonal 0 $ V.replicate (length $ summandsO o) 1) 0]
 
 
-scalarMorphism s = toMorphism (M.fromLists [[s]])
+projection :: Object -> Int -> Morphism
+projection o i =
+  let
+    len = length $ summandsO $ substO $ S.treeLabel
+      $ S.initialEdgeTree $ S.IV S.Main
+  in
+    Morphism [TauMatrix  (M.diagonal 0
+                         $ V.fromList ([1] ++ replicate (len - 1) 0)) 0]
 
 
-groupSum :: (GroupElement -> Scalar) -> Morphism
-groupSum f = toMorphism $ M.diagonal 0 $ V.generate order (f . GroupElement)
+-- scalarMorphism s = toMorphism (M.fromLists [[s]])
 
-morFromFunction :: (GroupElement -> GroupElement -> Scalar) -> Sum Int -> Morphism
+
+groupSum :: (AElement -> Scalar) -> Morphism
+groupSum f =  M.diagonal 0 $ V.generate order (f . AElement)
+
+morFromFunction :: (AElement -> AElement -> Scalar) -> Sum Int -> Morphism
 morFromFunction f exp = let
-  f2 (x,y) =  f (GroupElement $ x + 1) (GroupElement $ y + 1)
+  f2 (x,y) =  f (AElement $ x + 1) (AElement $ y + 1)
   in
     Morphism [TauMatrix (M.matrix order order f2) exp]
 
@@ -203,10 +238,10 @@ directSum x y =
 
 starSO :: SimpleObject -> SimpleObject
 starSO M =  M
-starSO (GE g) = GE (invert g)
+starSO (AE g) = AE (invert g)
 
 star :: Object -> Object
-star (SumO sos) = SumO $ map starSO sos
+star (Object sos) = Object $ map starSO sos
 
 
 -- https://en.wikipedia.org/wiki/Kronecker_product
@@ -234,105 +269,169 @@ kronecker f g =
     in
       (f M.! (r,s)) * (g M.! (v,w))
 
+expandRows :: [Int] -> M.Matrix a -> Int -> M.Matrix a
+expandRows indices m multiple =
+  let list = M.toLists m in
+    (take index list)
+    ++ repeat multiple (list !! index)
+    ++ drop index list
 
+
+
+expandColumn :: Int -> M.Matrix a -> Int -> M.Matrix a
+expandColumn index m multiple =
+  transpose $ expandRow (transpose M) index multiple
+
+-- Go through the direct sum of simple objects in the domain and range
+-- and check if each pair is (M,M)
 tensorM :: Morphism -> Morphism -> Morphism
 tensorM (Morphism tms1) (Morphism tms2) = Morphism
-  [TauMatrix (kronecker (matrix tm1) (matrix tm2))
-   ((tauExponent tm1) + (tauExponent tm2))
+  [
+    let
+      domPairs = [ (so1, so2) | so1 <- summandsO $ domain tm1
+                              , so2 <- summandsO $ domain tm2
+                              ]
+      codPairs = [ (so1, so2) | so1 <- summandsO $ codomain tm1
+                              , so2 <- summandsO $ codomain tm2
+                              ]
+    in
+      TauMatrix
+      { domain = (domain tm1) `tensorO` (domain tm2)
+      , codomain = (codomain tm1) `tensorO` (codomain tm2)
+      , matrix = (kronecker (matrix tm1) (matrix tm2))
+      , tauExponent = ((tauExponent tm1) + (tauExponent tm2))
+
+  ]
+    }
   | tm1 <- tms1
   , tm2 <- tms2]
 
 
 -- validInitialLabels :: S.InitialEdge -> [SimpleObject]
--- validInitialLabels ie = [M, GE $ GEVar ie]
+-- validInitialLabels ie = [M, AE $ AEVar ie]
 
 tensorSO :: SimpleObject -> SimpleObject -> Object
-tensorSO M M = SumO $ map GE group
-tensorSO M (GE _) = so M
-tensorSO (GE _) M = so M
-tensorSO (GE g1) (GE g2) = so $ GE $ g1 `mappend` g2
+tensorSO M M = Object $ map AE group
+tensorSO M (AE _) = so M
+tensorSO (AE _) M = so M
+tensorSO (AE g1) (AE g2) = so $ AE $ g1 `mappend` g2
 
---TODO: figure out how to flatten nested direct sums
+-- TODO: figure out how to flatten nested direct sums
 -- Should be able to use the fact that they are independent sums
 tensorO :: Object -> Object -> Object
-tensorO (SumO sos1) (SumO sos2) = SumO $
+tensorO (Object sos1) (Object sos2) = Object $
   concat $ map summandsO
   [ so1 `tensorSO` so2 | so1 <- sos1
                        , so2 <- sos2
   ]
 
--- FIXME
-initialLabel :: S.InitialEdge -> Object
-initialLabel ie = so $ GE (GroupElement 0)
 
+------------------------------------------------------
+--  Initial labels
+------------------------------------------------------
+
+-- FIXME
+initialLabel :: S.InitialEdge -> Object 
+initialLabel ie = -- so $ AE (AElement 0)
+  case ie of
+    S.LeftLoop -> so $ M
+    S.RightLoop -> so $ M --AE (AElement 1)
+    S.LeftLeg -> so $ M --AE (AElement 1)
+    S.RightLeg -> so $ M --AE (AElement 1)
+
+phi = projection
+      (substO $ S.treeLabel $ S.initialEdgeTree $ S.IV S.Main)
+      0
+
+
+
+-- initialLabel :: S.InitialEdge -> Object 
+-- initialLabel ie = 
+--   case ie of
+--     S.LeftLoop -> so $ AE (AElement 1)
+--     S.RightLoop -> so $ AE (AElement 1)
+--     S.LeftLeg -> so $ AE (AElement 1)
+--     S.RightLeg -> so $ AE (AElement 1)
+
+
+-- phi =  idMorphism $ substO $ S.treeLabel
+--   $ S.initialEdgeTree $ S.IV S.Main
+
+
+
+------------------------------------------------------
+--  Substituting TY labels for arbitrary ones
+------------------------------------------------------
+
+      
 -- Substitute in the TY-specific objects.
 substO :: S.Object -> Object
 substO o0 =  case o0 of
   S.OVar ie -> initialLabel ie
-  S.One -> so $ GE (GroupElement 0)
+  S.One -> so $ AE (AElement 0)
   S.Star o -> star $ substO o
   S.TensorO o1 o2 -> (substO o1) `tensorO` (substO o2)
 
 alpha :: SimpleObject -> SimpleObject -> SimpleObject -> Morphism
-alpha (GE g1) (GE g2) (GE g3) = idMorphism $ so $ GE $ g1 `mappend` g2 `mappend` g3
-alpha (GE _) (GE _) M = idMorphism $ so M
-alpha M (GE _) (GE _) = idMorphism $ so M
-alpha (GE a) M (GE b) = scalarMorphism (chi a b)
-alpha (GE _) M M = groupSum (\_ -> 1)
-alpha M M (GE _) = groupSum (\_ -> 1)
-alpha M (GE a) M = groupSum (\b -> chi a b)
+alpha (AE g1) (AE g2) (AE g3) = idMorphism $ so $ AE $ g1 `mappend` g2 `mappend` g3
+alpha (AE _) (AE _) M = idMorphism $ so M
+alpha M (AE _) (AE _) = idMorphism $ so M
+alpha (AE a) M (AE b) = scalarMorphism (chi a b)
+alpha (AE _) M M = groupSum (\_ -> 1)
+alpha M M (AE _) = groupSum (\_ -> 1)
+alpha M (AE a) M = groupSum (\b -> chi a b)
 alpha M M M = morFromFunction (\x y -> (chiI x y)) 1
 
 
 alphaI :: SimpleObject -> SimpleObject -> SimpleObject -> Morphism
-alphaI (GE g1) (GE g2) (GE g3) = idMorphism $ so $ GE $ g1 `mappend` g2 `mappend` g3
-alphaI (GE _) (GE _) M = idMorphism $ so M
-alphaI M (GE _) (GE _) = idMorphism $ so M
-alphaI (GE a) M (GE b) = scalarMorphism (chiI a b)
-alphaI (GE _) M M = groupSum (\x -> 1)
-alphaI M M (GE _) = groupSum (\x -> 1)
-alphaI M (GE a) M = groupSum (\b -> chiI a b)
+alphaI (AE g1) (AE g2) (AE g3) = idMorphism $ so $ AE $ g1 `mappend` g2 `mappend` g3
+alphaI (AE _) (AE _) M = idMorphism $ so M
+alphaI M (AE _) (AE _) = idMorphism $ so M
+alphaI (AE a) M (AE b) = scalarMorphism (chiI a b)
+alphaI (AE _) M M = groupSum (\x -> 1)
+alphaI M M (AE _) = groupSum (\x -> 1)
+alphaI M (AE a) M = groupSum (\b -> chiI a b)
 alphaI M M M = morFromFunction (\x y -> (chi x y)) (-1)
 
 coev :: SimpleObject -> Morphism
-coev M = toMorphism $ M.matrix 1 order $ \ij ->
+coev M = toMorphism $ M.matrix order 1 $ \ij ->
   if ij == (1,1)
   then 1
   else 0
-coev (GE _) = scalarMorphism 1
+coev (AE _) = scalarMorphism 1
 
 ev :: SimpleObject -> Morphism
-ev M =  Morphism [TauMatrix (M.matrix order 1 $ \ij ->
+ev M =  Morphism [TauMatrix (M.matrix 1 order $ \ij ->
   if ij == (1,1)
   then 1
   else 0)
   (-1)]
-ev (GE _) = scalarMorphism 1
+ev (AE _) = scalarMorphism 1
 
 pivotalJ :: SimpleObject -> Morphism
 pivotalJ M = scalarMorphism nu
-pivotalJ (GE _) = scalarMorphism 1
+pivotalJ (AE _) = scalarMorphism 1
 
 pivotalJI :: SimpleObject -> Morphism
 pivotalJI M = scalarMorphism nu
-pivotalJI (GE _) = scalarMorphism 1
+pivotalJI (AE _) = scalarMorphism 1
+
 
 composeTM :: TauMatrix -> TauMatrix -> TauMatrix
 composeTM tm1 tm2 =
   TauMatrix ((matrix tm1) * (matrix tm2))
   ((tauExponent tm1) + (tauExponent tm2))
 
+-- FIXME
 -- Assume all summands match up 
 compose :: Morphism -> Morphism -> Morphism
 compose (Morphism tms1) (Morphism tms2) = Morphism $
   zipWith composeTM tms1 tms2
   
--- TODO: Figure out where the program is hanging
 -- Substitute in the TY-specific morphisms
 substM :: S.Morphism -> Morphism
 substM m = case m of
-  S.Phi -> idMorphism $ substO $ S.treeLabel
-           $ S.initialEdgeTree $ S.IV S.Main
+  S.Phi -> phi
   S.Id o -> idMorphism $ substO o
   S.Lambda o -> idMorphism $ substO o
   S.LambdaI o -> idMorphism $ substO o
@@ -354,3 +453,13 @@ substM m = case m of
   S.PivotalJ  o -> sconcat $ N.fromList $ map pivotalJ  $ summandsO $ substO o
   S.PivotalJI o -> sconcat $ N.fromList $ map pivotalJI $ summandsO $ substO o
   S.Compose m1 m2 -> compose (substM m1) (substM m2)
+
+
+-- -- Debugging strategies
+-- -- 
+-- -- 1. finalMorphism (Look at subtrees)
+-- --
+-- -- 2. Look at TY code for error
+-- -- (Direct sum stuff).  In particular, add conceptual types to wrap
+-- -- around the computational types (i.e. SimpleObject functions to wrap
+-- -- around the matrices)

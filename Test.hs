d47f8446e81865d74b8913d90e01e74cd8b6f5e7
Paul Gustafson <paul.gustafson@gmail.com>
2017-04-01 15:57:39 -0500

    Added snake equation test

--- Test.hs
+++ Test.hs
@@ -5,6 +5,24 @@ import Stringnet as S
 import Data.Tree as T
 import Data.Matrix as M
 import Data.Maybe
+import Finite
+import Algebra
+
+
+obj = (toObject M) `TY.tensorO` (toObject M) `TY.tensorO` (toObject M)
+
+-- snake equations
+snake o = idMorphism o == ((ev o) `TY.tensorM` (idMorphism o))
+  `TY.compose` alpha o o o
+  `TY.compose` ((idMorphism o) `TY.tensorM` (coev o))
+
+
+
+
+
+-- 81 is interesting 
+-- finalET =  map (\ib -> map (substO (initialLabel ib)) $ map (S.objectLabel S.finalSN) $ S.flatten S.finalEdgeTree) (allElements :: [InitialBasisElement])
+
 
 -- old (finalMorphism) testing
 
@@ -22,16 +40,16 @@ leaves = catMaybes $ T.flatten $ toTree S.finalMorphism
 leftT (TensorM a b) = a
 rightT (TensorM a b) = b
 
-leftC (Compose a b) = a
-rightC (Compose a b) = b
+-- leftC (Compose a b) = a
+-- rightC (Compose a b) = b
 
-bad = Compose (AlphaI (Star (OVar RightLoop)) (OVar RightLoop) (Star One)) (Compose (RhoI (OVar RightLoop)) (Coev (OVar RightLoop)))
-
-
-small = (Compose (TensorM (PivotalJI (Star (OVar RightLoop))) (LambdaI (OVar RightLoop))) (Coev (OVar RightLoop)))
+-- bad = Compose (AlphaI (Star (OVar RightLoop)) (OVar RightLoop) (Star One)) (Compose (RhoI (OVar RightLoop)) (Coev (OVar RightLoop)))
 
 
--- new testing
+-- small = (Compose (TensorM (PivotalJI (Star (OVar RightLoop))) (LambdaI (OVar RightLoop))) (Coev (OVar RightLoop)))
 
--- TODO: Calculate a matrix for addCoev.  What I need to do is figure
--- out how to turn the monad actions into a list of actions
+
+-- -- new testing
+
+-- -- TODO: Calculate a matrix for addCoev.  What I need to do is figure
+-- -- out how to turn the monad actions into a list of actions.  

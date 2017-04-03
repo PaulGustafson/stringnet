283a0d5e384441736e0e50f1f60c01f7df99ee2f
Paul Gustafson <paul.gustafson@gmail.com>
2017-04-03 10:24:01 -0500

    Added rmatrix printing

--- Main.hs
+++ Main.hs
@@ -2,7 +2,20 @@ module Main where
 
 import Stringnet
 import TambaraYamagami
+import Data.Matrix
+import Data.List
+--import Test
 
 main :: IO ()
-main = print $ finalMorphism
+-- main = print $ finalMorphism
 -- main = print $ substM finalMorphism
+-- main = print $ toTree finalMorphism
+-- main = print $ substM $ leaves !! 0
+main = putStrLn $ latex $ rmatrix
+-- main = print rmatrix
+
+
+latex :: Show a => Matrix a -> String
+latex m =
+  intercalate " \\\\ \n" . map (intercalate  " & ". map show) $ toLists m
+
